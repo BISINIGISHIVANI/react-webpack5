ae64191d14be7197d05d0ff1a37570bdb7ee752c
BISINIGISHIVANI <bisinigishivani@gmail.com>
2022-08-30 12:38:55 +0530

    add webpack,other packeges

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import logo from "./logo.svg"
+const App=()=>{
+	return (
+		<div className="App">
+			<header className="App-header">
+			<img src={logo} alt="logo" className="App-logo" />
+       		 <p>
+       		   Edit <code>src/App.js</code> and save to reload.
+       		 </p>
+			<h2>Webpack React App</h2>
+      </header>
+		</div>
+	);
+}
+
+export default App;
