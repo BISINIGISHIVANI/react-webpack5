2e6447ce09d1fa0d1e988aeeb32612c3f3200df5
BISINIGISHIVANI <bisinigishivani@gmail.com>
2022-09-13 15:37:12 +0530

    update app with jsx extension

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App=()=>{
 			<header className="App-header">
 			<img src={logo} alt="logo" className="App-logo" />
        		 <p>
-       		   Edit <code>src/App.js</code> and save to reload.
+       		   Edit <code>src/App.jsx</code> and save to reload.
        		 </p>
 			<h2>Webpack React App</h2>
       </header>
